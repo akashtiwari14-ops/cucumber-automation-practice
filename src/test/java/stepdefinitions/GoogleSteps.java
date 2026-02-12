946cbefc51a0a8e4b13033b40912dc859a5725fc
akash tiwari <ATiwari@biointellisense.com>
2026-02-12 16:38:50 +0530

    Added browser parameter support for Jenkins

--- src/test/java/stepdefinitions/GoogleSteps.java
+++ src/test/java/stepdefinitions/GoogleSteps.java
@@ -5,12 +5,17 @@ import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.DriverFactory;
 
 import static org.junit.Assert.assertTrue;
+
+import java.time.Duration;
 
 public class GoogleSteps {
 
@@ -22,19 +27,30 @@ public class GoogleSteps {
     	
     	String browser = System.getProperty("browser", "chrome");
 
+    	ChromeOptions options = new ChromeOptions();
+
     	if(browser.equalsIgnoreCase("headless")){
-    	    ChromeOptions options = new ChromeOptions();
     	    options.addArguments("--headless=new");
-    	    driver = new ChromeDriver(options);
-    	}else{
-    	    driver = new ChromeDriver();
+    	    options.addArguments("--no-sandbox");
+    	    options.addArguments("--disable-dev-shm-usage");
     	}
+
+    	driver = new ChromeDriver(options);
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    	driver.manage().window().maximize();
     }
 
     @When("User searches for {string}")
     public void searchFor(String searchText) {
-        driver.findElement(By.name("q"))
-                .sendKeys(searchText + Keys.ENTER);
+//        driver.findElement(By.name("q"))
+//                .sendKeys(searchText + Keys.ENTER);
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    	WebElement searchBox = wait.until(
+    	        ExpectedConditions.visibilityOfElementLocated(By.name("q"))
+    	);
+
+    	searchBox.sendKeys(searchText + Keys.ENTER);
     }
 
     @Then("User should see page title containing {string}")
